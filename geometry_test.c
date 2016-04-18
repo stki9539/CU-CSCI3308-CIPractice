a04c57fb7ac1b84a2a3405c7ef2664248ac2cac9
steven kim <steven.kim@colorado.edu>
2016-04-18 13:56:34 -0600

    added area test function

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,24 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+START_TEST(test_2d_area)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+
+    a.x= 5;
+    a.y= 5;
+    b.x= 5;
+    b.y= 25;
+    c.x= 15;
+    c.y=5;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 100.0);
+
+}
+END_TEST
+
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -161,12 +179,15 @@ Suite* coord_2d_suite(void)
 
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
-
+    
+    TCase* tc_2d_area = tcase_create("coord_2d_area");
+    tcase_add_test(tc_2d_area, test_2d_area);
+    
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
-
+    suite_add_tcase(s, tc_2d_area);
     /* Return Suite */
     return s;
 
